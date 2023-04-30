606cb5ba2ed18eb00ccfd48b46653bb28923f01c
sumit <uxdlabsumit@gmail.com>
2023-04-30 12:12:59 +0530

    add class demo

---
+++ src/main/java/gitcode/Demo1.java
@@ -0,0 +1,19 @@
+package gitcode;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com");
+
+	}
+
+}
